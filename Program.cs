68d1ea50b29dd7bbbfac8739cf75e352ad0a2086
GreKffwfwkepob <ahtulov06@gmail.com>
2024-11-16 19:50:31 +0300

    Intial commit with menu options

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("Enter A");
+            Console.WriteLine("Enter B");
+            Console.WriteLine("Perform the operation \"+\"");
+            Console.WriteLine("Perform the operation \"-\"");
+            Console.WriteLine("Perform the operation \"*\"");
+            Console.WriteLine("Perform the operation \"/\"");
+        }
+
+    }
+}
+
